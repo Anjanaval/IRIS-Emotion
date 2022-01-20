0bedba75ec0c289bc698f811dfc03c4116629bc1
Anjana Valsalan <valsalananjana@gmail.com>
2022-01-19 22:19:33 -0400

    Fixed model loading

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('iris_emotion/')
+
 setup(
   name = 'iris_emotion',        
   packages = ['iris_emotion'],
-  include_package_data= True,   
-  version = '0.1.1',      
+  include_package_data= True,
+  package_data={'': extra_files},   
+  version = '0.2.0',      
   license='MIT',       
   description = 'Detect emotions in text.',
   long_description=long_description,
